f9f008474529aa0065b6872a2f0a8f06d164b823
ossuchas <suchat.s14102526@gmail.com>
2020-04-11 07:40:54 +0700

    added#3

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,22 +1,32 @@
 <template>
     <div class="container">
-        <div class="display-4 text-center">Test by Suchat {{info}}</div>
-        <p>{{ info }} + {{ url }}</p>
-        <p>Hello World Test Jenkins file 1.0.3</p>
+        <div class="display-4 text-center">{{info}}</div>
+        <!-- <p>{{ info }} + {{ url }}</p> -->
+        <div class="tableauPlaceholder" style="width: 1000px; height: 827px;">
+            <iframe :src="url" width="100%" height="100%"></iframe>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
    name: 'Test',
    data() {
        return {
-           info: 'xxx',
-           url: process.env.VUE_APP_URL
+           info: 'Test Tableau Viewer',
+           url: null
        }
    },
    mounted() {
        console.log('kai :: ' + process.env.VUE_APP_URL)
+       
+       axios.get('https://test-apchatbotapi.apthai.com/api/v1/genticket')
+        .then(response => {
+            console.log(response)
+            this.url = "http://dashboard.apthai.com/trusted/" + response.data['message']+ "/t/CRM/views/LLWalkByBG/DS_LLWalkBG?:embed=yes&:toolbar=no&:tabs=no"
+        });
    }
 }
 </script>
