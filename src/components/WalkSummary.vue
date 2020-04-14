50aa93299e55f1a9bfbcff6f1cd72fa7c06e9836
ossuchas <suchat.s14102526@gmail.com>
2020-04-14 19:25:10 +0700

    add new component and router

--- src/components/WalkSummary.vue
+++ src/components/WalkSummary.vue
@@ -1,10 +1,32 @@
 <template>
-    <div>Walk Summary by BG</div>
+    <v-container>
+        <v-row class="text-center">
+            <v-col cols="12">
+                <iframe class="embed-responsive-item resp-iframe" :src="url" width="375" height="1000px" frameborder="0" scrolling="no" allowfullscreen></iframe>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
+// import axios from 'axios';
+import axios from 'axios';
+
 export default {
-    
+   name: 'WalkSummary',
+   data() {
+       return {
+           ticket: '',
+           url: ''
+       }
+   },
+   mounted() {
+       axios.get('/genticket')
+        .then(response => {
+            var ticket = response.data['message'];
+            this.url = "http://dashboard.apthai.com/trusted/" + ticket + "/t/CRM/views/LLWalkByBG/Res_WalkBG2?:refresh=true&:embed=yes&:toolbar=no&:tabs=no"
+        });
+   }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,71 +1,70 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <button @click="openWindow()" class="btn btn-primary">Open Window</button>
-    <button @click="sendMessage()" class="btn btn-secondary">Send Message</button>
-    <button @click="getProfile()" class="btn btn-danger">Get Profile</button>
-    <div>
-      <p>{{ profile.userId || '-' }}</p>
-      <p>{{ profile.displayName || '-' }}</p>
-      <p>{{ profile.pictureUrl|| '-' }}</p>
-      <p>{{ profile.statusMessage|| '-' }}</p>
+  <v-container class="text-center">
+    <div class="home">
+      <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+      <button @click="openWindow()" class="btn btn-primary">Open Window</button>
+      <button @click="sendMessage()" class="btn btn-secondary">
+        Send Message
+      </button>
+      <button @click="getProfile()" class="btn btn-danger">Get Profile</button>
+      <div>
+        <p>{{ profile.userId || "-" }}</p>
+        <p>{{ profile.displayName || "-" }}</p>
+        <p>{{ profile.pictureUrl || "-" }}</p>
+        <p>{{ profile.statusMessage || "-" }}</p>
+      </div>
     </div>
-  </div>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     HelloWorld
   },
   data: () => ({
     profile: {
-      userId: '',
-      displayName: '',
-      pictureUrl: '',
-      statusMessage: ''
+      userId: "",
+      displayName: "",
+      pictureUrl: "",
+      statusMessage: ""
     }
   }),
   methods: {
-    openWindow () {
+    openWindow() {
       this.$liff.openWindow({
-        url: 'https://developers.line.me/en/docs/liff/overview/'
-      })
+        url: "https://developers.line.me/en/docs/liff/overview/"
+      });
     },
-    closeWindow () {
-      this.$liff.closeWindow()
+    closeWindow() {
+      this.$liff.closeWindow();
     },
-    async getProfile () {
-      let _this = this
-      await this.$liff.getProfile().then(function (profile) {
-        _this.profile = profile
-      }).catch(function (error) {
-        alert('Error getting profile: ' + error)
-      })
+    async getProfile() {
+      let _this = this;
+      await this.$liff
+        .getProfile()
+        .then(function(profile) {
+          _this.profile = profile;
+        })
+        .catch(function(error) {
+          alert("Error getting profile: " + error);
+        });
     },
-    async sendMessage () {
-      // this.$liff.sendMessages([ {
-      //     type: 'text',
-      //     text: 'You/\'ve successfully sent a message! Hooray!'
-      //   }
-      // ]).then(function () {
-      //   window.alert('Message sent')
-      // }).catch(function (error) {
-      //   window.alert('Error sending message: ' + error)
-      // })
-      await this.$liff.sendMessages([{
-          type: 'text',
-          text: 'test'
+    async sendMessage() {
+      await this.$liff.sendMessages([
+        {
+          type: "text",
+          text: "test"
         }
       ]);
-      
-      this.$liff.closeWindow()
+
+      this.$liff.closeWindow();
     }
   }
-}
+};
 </script>
