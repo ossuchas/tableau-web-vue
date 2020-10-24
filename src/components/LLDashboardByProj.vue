1fef1f472ae087fe7d24deb8146f44ea2e0da493
เรารักเมียหลวง <>
2020-10-24 18:34:27 +0700

    modified#2

--- src/components/LLDashboardByProj.vue
+++ src/components/LLDashboardByProj.vue
@@ -23,12 +23,13 @@ export default {
    },
    mounted() {
        this.userId = this.$route.query.userId
+    //    console.log('kai => ' + this.userId)
     //    this.userid = "U087b2673fb770b30b0ff1f8bd2768451"
        /* API Get ticket to authorized tableau */
        axios.get('/genticket')
         .then(response => {
             var ticket = response.data['message'];
-            this.url = process.env.VUE_APP_TABLEAU_URL + "/" + ticket + "/t/CRM/views/LLDashboard/LLDashboardByProj?:refresh=true&:embed=yes&:toolbar=no&:tabs=no&userId=" + this.userid;
+            this.url = process.env.VUE_APP_TABLEAU_URL + "/" + ticket + "/t/CRM/views/LLDashboard/LLDashboardByProj?:refresh=true&:embed=yes&:toolbar=no&:tabs=no&userId=" + this.userId;
         });
    },
    created() {
