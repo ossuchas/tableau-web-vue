7dc2d19196fb11947adb03f5ff4d0c17e9b974fc
เรารักเมียหลวง <>
2020-12-19 20:33:30 +0700

    modified#2

--- src/components/ExecutiveDS/DSExecutiveWTD.vue
+++ src/components/ExecutiveDS/DSExecutiveWTD.vue
@@ -6,7 +6,7 @@
           class="embed-responsive-item resp-iframe"
           :src="url"
           width="375"
-          height="1000px"
+          height="1500px"
           frameborder="0"
           scrolling="no"
           allowfullscreen
